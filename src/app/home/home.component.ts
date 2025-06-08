c1a78971b246a6f6356a1c174d1f0e696e2461f2
Mohan <mohan8105072435@gmail.com>
2025-06-08 18:09:24 +0530

    home component design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { CustomSnackbarService } from '../shared/custom-snackbar.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +6,5 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
-  constructor(
-    private snackbar: MatSnackBar,
-        private snackbarService: CustomSnackbarService
-  ) {
-  }
-  openSnackBar(): void {
-       this.snackbarService.open('Logged in successfully!', 'error');
-  }
-
-  openCustomSnackBar(): void {
-    this.snackbarService.open('Logged in successfully!', 'success');
-  }
+  currentYear = new Date().getFullYear();
 }
